8375fa1ee246809381f04a1ddc8cf573d5b0fe95
D0masss <smikis423@gmail.com>
2026-06-09 14:55:58 +0300

    update API endpoints in CitySelection and FollowingFlights components to use relative paths

--- src/pages/CitySelection.jsx
+++ src/pages/CitySelection.jsx
@@ -38,7 +38,7 @@ const CitySelection = () => {
 
   const mutation = useMutation({
     mutationFn: async (data) => {
-      const response = await fetch("http://63.184.22.134/search-flights", {
+      const response = await fetch("/search-flights", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
